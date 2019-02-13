a87fc8e54101ed803d36e7f6c58a3b2bb6669e86
David <david.sato64@gmail.com>
2019-02-13 17:17:26 +0200

    balance formatting

--- src/components/display/Home.jsx
+++ src/components/display/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { connect } from '../StateProvider'
 
+import { mapTS, fromWei } from '../../api/utils'
+
 const Home = ({ account, balance: userBalance, network }) => (
   <div>
     <h1>Provider state:</h1>
 
     <h3>Network: {network}</h3>
     <h3>Account: {account}</h3>
-    <h3>Balance: {userBalance}</h3>
+    <h3>Balance: {mapTS(fromWei(userBalance))}</h3>
   </div>
 )
 
