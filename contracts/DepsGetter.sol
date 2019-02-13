2cbe3787d35901bcdbea655397eb8a2b64aa6bbf
David <david.sato64@gmail.com>
2019-02-13 16:46:03 +0200

    renamed Getter.sol to DepsGetter.sol

---
+++ contracts/DepsGetter.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.2;
+
+// import "@gnosis.pm/dx-contracts/contracts/DxDevDependencies.sol";
+import "@gnosis.pm/dx-mgn-pool/contracts/DxMgnPoolDepsGetter.sol";
+import "@gnosis.pm/dx-mgn-pool/contracts/Coordinator.sol";
+
+
+contract DepsGetter {}
