b39968b97fd8e6e91c84c19a74abb5c48e5a27b3
David <david.sato64@gmail.com>
2019-02-14 16:20:47 +0200

    fixed truffleConfig
    1. hdwallet back to nonbrekaing version <1
    2. use util-contracts truffleConfig
    3. passed in registered whitelisted infura addresses

--- src/globals.js
+++ src/globals.js
@@ -26,3 +26,8 @@ export const WEBSOCKET_URLS = {
   ROPSTEN: 'wss://ropsten.infura.io/ws/v3/fb2b930672ff4872bfcad69671f2dfd4',
   LOCAL: 'ws://localhost:8545/ws',
 }
+
+export const INFURA_URLS = {
+  MAIN: 'https://mainnet.infura.io/v3/fb2b930672ff4872bfcad69671f2dfd4',
+  RINKEBY: 'https://rinkeby.infura.io/v3/fb2b930672ff4872bfcad69671f2dfd4',
+}
