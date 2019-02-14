73e03f1f547b99df6f698d3cf9e515fbc190d0ae
David <david.sato64@gmail.com>
2019-02-14 11:07:13 +0200

    added websocket urls

--- src/globals.js
+++ src/globals.js
@@ -17,3 +17,12 @@ export const networkById = {
 
 export const GAS_LIMIT = 4000000
 export const GAS_PRICE = 5e9
+
+export const WEBSOCKET_URLS = {
+  MAIN: 'wss://mainnet.infura.io/ws/v3/fb2b930672ff4872bfcad69671f2dfd4',
+  RINKEBY: 'wss://rinkeby.infura.io/ws/v3/fb2b930672ff4872bfcad69671f2dfd4',
+  KOVAN: 'wss://kovan.infura.io/ws/v3/fb2b930672ff4872bfcad69671f2dfd4',
+  MORDEN: 'wss://morden.infura.io/ws/v3/fb2b930672ff4872bfcad69671f2dfd4',
+  ROPSTEN: 'wss://ropsten.infura.io/ws/v3/fb2b930672ff4872bfcad69671f2dfd4',
+  LOCAL: 'ws://localhost:8545/ws',
+}
