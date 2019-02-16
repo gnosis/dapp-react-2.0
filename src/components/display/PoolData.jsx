27c5beafdd330049c5235b100170805c8eb0f71f
David <david.sato64@gmail.com>
2019-02-16 16:39:59 +0200

    added tokenName/symbol into pool data

--- src/components/display/PoolData.jsx
+++ src/components/display/PoolData.jsx
@@ -14,7 +14,7 @@ const PoolData = ({
                 <div className="poolInnerContainer">
                     {/* POOL 1 */}
                     <pre className="poolDataContainer data-pre-blue">
-                        <h2>DxMGNPool #1 {/* TOKEN SYMBOL HERE */}</h2>
+                        <h2>DxMGNPool #1 - {DX_MGN_POOL.pool1.dtName} [{DX_MGN_POOL.pool1.dtSymbol}]</h2>
                         <ul>
                             {Object.keys(pool1).map((key, idx) => <li key={idx * Math.random()}>{`${key}: ${cleanData(pool1[key])}`}</li>)}
                         </ul>
@@ -22,7 +22,7 @@ const PoolData = ({
 
                     {/* POOL 2 */}
                     <pre className="poolDataContainer data-pre-green">
-                        <h2>DxMGNPool #2 {/* TOKEN SYMBOL HERE */}</h2>
+                        <h2>DxMGNPool #2 {DX_MGN_POOL.pool1.stName} [{DX_MGN_POOL.pool1.stSymbol}]</h2>
                         <ul>
                             {Object.keys(pool2).map((key, idx) => <li key={idx * Math.random()}>{`${key}: ${cleanData(pool2[key])}`}</li>)}
                         </ul>
