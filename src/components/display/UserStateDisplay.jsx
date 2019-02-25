cc1958fd673b12048a5a435a0f750f82c69b1b0a
David <david.sato64@gmail.com>
2019-02-25 16:44:25 +0200

    ramped up UserStateDisplay

--- src/components/display/UserStateDisplay.jsx
+++ src/components/display/UserStateDisplay.jsx
@@ -1,66 +1,63 @@
 import React from 'react'
 
-import DataDisplayVisualContainer, { DataDisplay } from './DataDisplay'
+import DataDisplayVisualContainer from './DataDisplay'
 import AsyncActionsHOC from '../hoc/AsyncActionsHOC'
 
 import {
   lockAllMgn,
 } from '../../api'
 
-import { 
-  AccountSubscription, 
-  AccountSub, 
-  ETHbalanceSubscription, 
-  ETHbalanceSub, 
-  MGNBalancesSubscription, 
-  MGNBalancesSub,
-} from '../../subscriptions'
-
-import Provider, { connect } from '../StateProvider'
+import { connect } from '../StateProvider'
 
 const LockMGN = AsyncActionsHOC()
 
-const UserStateDisplay = ({ NETWORK }) =>
+const UserStateDisplay = ({ state, NETWORK, USER, MGN_BALANCES }) =>
   <DataDisplayVisualContainer
     title="Connected Wallet"
     colour="violet"
   >
-    {otherProps =>
+    {() =>
     <>
       <h5>Account + netWoRk</h5>
-
-      <AccountSubscription source={AccountSub}>
-        {subValue => <DataDisplay {...subValue} {...otherProps} />}
-      </AccountSubscription>
-      <AccountSubscription source={AccountSub}>
-        {value => <Provider {...value} />}
-      </AccountSubscription>
-
+      <p>ACCOUNT: {USER.ACCOUNT}</p>
+      <p>[ETH] BALANCE: {USER.BALANCE && USER.BALANCE}</p>
       <p>NETWORK: {NETWORK}</p>
 
       <hr />
 
-      <h5>bAlances</h5>
-
-      <ETHbalanceSubscription source={ETHbalanceSub}>
-        {subValue => <DataDisplay {...subValue} {...otherProps} />}
-      </ETHbalanceSubscription>
-      
-      <MGNBalancesSubscription source={MGNBalancesSub}>
-        {subValue => <DataDisplay {...subValue} {...otherProps} />}
-      </MGNBalancesSubscription>
+      <h5>mgn bAlances</h5>
+      {Object.keys(MGN_BALANCES).map(key => <p key={key + Math.random()}>{key.toUpperCase().split('_').join(' ')}: {MGN_BALANCES[key]}</p>)}
       
       <LockMGN 
         asyncAction={lockAllMgn}
         buttonText="Lock"
         title="Lock Mgn Tokens"
       />
+      {JSON.stringify({ ...state.USER, ...state.DX_MGN_POOL, ...{ PROVIDER: { ACTIVE_PROVIDER: state.PROVIDER.ACTIVE_PROVIDER, NETWORK: state.PROVIDER.NETWORK } } }, undefined, 2)}
     </>
     }
   </DataDisplayVisualContainer>
 
-const mapState = ({ state: { PROVIDER: { NETWORK } } }) => ({
+const mapState = ({ 
+  state,
+  state: { 
+    USER, 
+    PROVIDER: { NETWORK }, 
+    TOKEN_MGN: {
+      MGN_BALANCE,
+      LOCKED_MGN_BALANCE,
+      UNLOCKED_MGN_BALANCE,
+    },
+  }, 
+}) => ({
+  state,
   NETWORK,
+  USER,
+  MGN_BALANCES: {
+    BALANCE: MGN_BALANCE,
+    LOCKED_BALANCE: LOCKED_MGN_BALANCE,
+    UNLOCKED_BALANCE: UNLOCKED_MGN_BALANCE,
+  },
 })
 
 export default connect(mapState)(UserStateDisplay)
