dbe4963b8424f2a6776ca5c5a1f8480638a1a0ac
David <david.sato64@gmail.com>
2019-02-27 10:15:33 +0200

    moved and renamed global CSS file + removed txt file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import './yourCSSHere.css'
+import './styles/global.css'
 
 ReactDOM.hydrate(<App />, document.getElementById('app'))
